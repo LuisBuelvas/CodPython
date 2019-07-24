03fd4a8c351b2995ff64c7ae32cdc002f2d26987
LuisAlberto <labuelvas1@misena.edu.co>
2019-07-24 10:58:30 -0500

    actualizar rename

--- rename.py
+++ rename.py
@@ -1,18 +1,27 @@
+# #!/usr/bin/python3
+# import os, sys
+
+# # Open a file
+# path = "/home/dev06/Descargas/prueba nombre/"
+# dirs = os.listdir( path )
+
+# # This would print all the files and director0ies
+# for file in dirs:
+#    print (file)
+
+
 
 import os
-import logging
-path = "/home/dev06/Descargas/prueba nombre/ok"
+path = "/home/dev06/Descargas/prueba nombre/"
 dirs = os.listdir( path )
 
 for fn in dirs:
     src = path + fn
-    dst = path + fn[:-8] +fn[-7:]
-    print(dst)
-    os.rename(src, dst)
-    
-#     if fn.endswith("-407.cam2") or fn.endswith('-407.cam1'):
-#         os.rename(src, dst)
-#         print("Objeto Renombrado")
-
-#logging.debug()
+    dst = path + fn[3:] 
+    if fn.startswith("ok_"):
+        os.rename(src, dst)
+print("Objeto Renombrado")
        
+# for filename in dirs:
+#      if filename.startswith("ok-"):
+#          os.rename(filename, filename[3:])
